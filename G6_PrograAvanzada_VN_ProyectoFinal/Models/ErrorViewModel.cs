65c0c5cc605a247df53b59c5926eef0b1b744e07
3RRR0RRR <162829597+3RRR0RRR@users.noreply.github.com>
2025-11-01 05:44:13 -0600

    Controller, Model, views
    Solo me falta la parte de bitacora

---
+++ G6_PrograAvanzada_VN_ProyectoFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace G6_PrograAvanzada_VN_ProyectoFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
