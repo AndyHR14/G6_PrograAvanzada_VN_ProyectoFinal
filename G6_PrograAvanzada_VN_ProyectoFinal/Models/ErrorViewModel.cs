237db0ff7456cae3dc964318265cd0fff4ff9842
3RRR0RRR <162829597+3RRR0RRR@users.noreply.github.com>
2025-11-05 01:29:32 -0600

    Revert "Controller, Model, views"
    This reverts commit 65c0c5cc605a247df53b59c5926eef0b1b744e07.

--- G6_PrograAvanzada_VN_ProyectoFinal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace G6_PrograAvanzada_VN_ProyectoFinal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
