e6ee72b8db04100759728a4a16a194dcc663154b
AndyHR14 <andypipoh@gmail.com>
2025-11-05 22:27:22 -0600

    Cajas avance

---
+++ ProyectoFinal/Models/Cajas.cs
@@ -0,0 +1,17 @@
+﻿namespace ProyectoFinal.Models
+{
+    public class Cajas
+    {
+        public int IdCaja {  get; set; }
+        public int IdComercio { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion {  get; set; }
+        public string TelefonoSINPE { get; set; }
+        public DateTime FechaDeRegistro {  get; set; }
+        public DateTime FechaDeModificacion { get; set; }
+        public bool Estado {  get; set; }
+
+        public ICollection<SINPE> SINPEs { get; set; }
+    }
+}
+
