bdbeb0fdac7055152adcf4ed83d7010e9f474aa3
Gabriel G.M <ggutierrez00078@ufide.ac.cr>
2025-11-28 15:58:09 -0600

    Git ignore

--- ProyectoFinal/obj/Debug/net9.0/ProyectoFinal.AssemblyInfo.cs
+++ ProyectoFinal/obj/Debug/net9.0/ProyectoFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd77485becfa9c58bf0d19ad3989a3ee10f30192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d31c1941c537ef8a7fa27bc1d2289228e0805cd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
